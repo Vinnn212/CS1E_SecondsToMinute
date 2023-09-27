93a32b5f54be2a61100c57cfbc82dd05e7816592
Vinnn212 <145567193+Vinnn212@users.noreply.github.com>
2023-09-27 22:30:41 +0800

    Add files via upload

---
+++ Santocildes_SecondsToMinutesConvert.py
@@ -0,0 +1,5 @@
+seconds = int(input("Enter the number of seconds:"))
+minutes = seconds // 60
+remaining_seconds = seconds % 60
+
+print(seconds, "seconds is", minutes, "minutes and", remaining_seconds, "seconds")
